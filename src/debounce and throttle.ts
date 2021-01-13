f6bb480d206ca6a735780e079dc64d06586d54bb
Moushudyx <70894864+Moushudyx@users.noreply.github.com>
2021-01-13 12:36:27 +0800

    添加了两个新的功能
    复合版的逻辑（从第一次点击开始，在一个阈值周期内）：
    点击一次 -> 立即执行
    点击两次 -> 立即执行一次，周期结束后执行第二次
    点击三次或以上 -> 同上
    复合板plus的逻辑（从第一次点击开始，在一个阈值周期内）：
    点击一次 -> 立即执行
    快速点击两次 -> 立即执行
    慢慢点击两次 -> 立即执行一次，周期结束后执行第二次
    * 若有一次触发落在nextStepThreshold外，则周期结束后执行第二次。
    * 好像有点绕，简单来说，若一个周期1s，第一次触发时立即执行，此后0.5s内的触发将被忽略，在0.5-1s的触发将被放在1s时执行。
    * 然后这个默认 50% 位置的参数可以改。

--- src/debounce and throttle.ts
+++ src/debounce and throttle.ts
@@ -1,12 +1,12 @@
 /**
  * 防抖函数, 防止用户在短时间内触发次数太多
  * @param handler 需要执行的方法
- * @param threshold 防抖阈值
+ * @param threshold 防抖阈值, 单位ms
  * @param _arguments 传给方法的参数
  */
 let debounce: (handler: Function, threshold?: number, _arguments?: Array<any>) => void
 // 防抖函数的简单实现, 必定执行最后一次操作, 但是不会立即响应
-debounce = function (handler: Function, threshold: number = 300, _arguments?: Array<any>) {
+debounce = function (handler, threshold = 300, _arguments) {
     let timeout: number | undefined
     return function () {
         clearTimeout(timeout)
@@ -18,18 +18,77 @@ debounce = function (handler: Function, threshold: number = 300, _arguments?: Ar
 /**
  * 节流函数, 防止用户在短时间内触发次数太多
  * @param handler 需要执行的方法
- * @param threshold 节流阈值
+ * @param threshold 节流阈值, 单位ms
  * @param _arguments 传给方法的参数
  */
 let throttle: (handler: Function, threshold?: number, _arguments?: Array<any>) => void
 // 节流函数的简单实现, 立即响应, 但是不一定执行最后一次操作
-throttle = function (handler: Function, threshold: number = 300, _arguments?: Array<any>) {
+throttle = function (handler, threshold = 300, _arguments) {
     let last: number | undefined
     return function () {
         let now = new Date().getTime()
         if (now - (last || 0) > threshold) {
             last = now
             if (_arguments) { handler(..._arguments) } else { handler() }
+        }
+    }
+}
+/**
+ * 统一的节流防抖函数, 防止用户在短时间内触发次数太多
+ * @param handler 需要执行的方法
+ * @param threshold 节流与防抖的阈值, 单位ms
+ * @param _arguments 传给方法的参数
+ */
+let throttledAndDebounce: (handler: Function, threshold?: number, _arguments?: Array<any>) => void
+// 节流防抖函数的简单实现, 立即响应, 且必定执行最后一次操作
+throttledAndDebounce = function (handler, threshold = 300, _arguments) {
+    let last: number | undefined
+    let timeout: number | undefined
+    return function () {
+        let now = new Date().getTime()
+        let timeDiff = now - (last || 0)
+        if (timeDiff > threshold) {
+            last = now
+            clearTimeout(timeout)
+            if (_arguments) { handler(..._arguments) } else { handler() }
+        } else {
+            clearTimeout(timeout)
+            timeout = setTimeout(() => {
+                if (_arguments) { handler(..._arguments) } else { handler() }
+            }, timeDiff)
+        }
+    }
+}
+/**
+ * 统一的节流防抖函数, 防止用户在短时间内触发次数太多
+ * 
+ * 默认在阈值的一半时间后再次触发, 视为需要执行的最后一次操作
+ * 
+ * 若阈值为1000, 二次触发阈值为0.5, 用户两次触发时间间隔大于500时, handler将执行两次
+ * @param handler 需要执行的方法
+ * @param threshold 节流与防抖的阈值, 单位ms
+ * @param _arguments 传给方法的参数
+ * @param nextStepThreshold 在冷却期内什么时候再次触发, 视为需要执行的最后一次操作, 小数表示, 默认 0.5 即 50%
+ */
+let throttledAndDebouncePlus: (handler: Function, threshold?: number, _arguments?: Array<any>, nextStepThreshold?: number) => void
+// 节流防抖函数的复杂实现, 立即响应, 在触发时间间隔较长时必定执行最后一次操作
+throttledAndDebouncePlus = function (handler, threshold = 300, _arguments, nextStepThreshold = 0.5) {
+    let last: number | undefined
+    let timeout: number | undefined
+    /**判定为有效的下一次操作时间间隔 */
+    let nextThreshold = threshold * nextStepThreshold
+    return function () {
+        let now = new Date().getTime()
+        let timeDiff = now - (last || 0)
+        if (timeDiff > threshold) {
+            last = now
+            clearTimeout(timeout)
+            if (_arguments) { handler(..._arguments) } else { handler() }
+        } else if (timeDiff > nextThreshold) {
+            clearTimeout(timeout)
+            timeout = setTimeout(() => {
+                if (_arguments) { handler(..._arguments) } else { handler() }
+            }, timeDiff)
         }
     }
 }
